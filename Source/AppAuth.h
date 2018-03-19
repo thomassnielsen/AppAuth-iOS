39bfbd8afa522bc63fac30c7b1ed836d36adb2e8
thomassnielsen <me@thomassnielsen.com>
2018-03-19 19:46:12 +0100

    Remove support for SFAuthenticationSession
    SFAuthenticationSession causes issues with logout

--- Source/AppAuth.h
+++ Source/AppAuth.h
@@ -72,7 +72,7 @@
 
     It follows the best practices set out in 
     [RFC 8252 - OAuth 2.0 for Native Apps](https://tools.ietf.org/html/rfc8252)
-    including using `SFAuthenticationSession` and `SFSafariViewController` on iOS
+    including using `SFSafariViewController` on iOS
     for the auth request. `UIWebView` and `WKWebView` are explicitly *not*
     supported due to the security and usability reasons explained in
     [Section 8.12 of RFC 8252](https://tools.ietf.org/html/rfc8252#section-8.12).
